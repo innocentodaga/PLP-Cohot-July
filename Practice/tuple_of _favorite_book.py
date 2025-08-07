f307287567d8e4557267b75c1285fe70f6a5c0e1
Dev-Inno <101554587+innocentodaga@users.noreply.github.com>
2025-08-07 19:31:30 +0300

    Tuple of Favorite Books and Print Each Book python Copy code

---
+++ Practice/tuple_of _favorite_book.py
@@ -0,0 +1,6 @@
+# a tuple of fav books
+favorite_books = ("The Essential Math for AI", "The Alchemist", "To Kill a Mockingbird", "Atomic Habits", "Sapiens")
+
+# display each book on a separate lane
+for book in favorite_books:
+    print(book)
