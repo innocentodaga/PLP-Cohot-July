a40c335eee2986d88b55a4fabf91c00fba9474f7
Dev-Inno <101554587+innocentodaga@users.noreply.github.com>
2025-08-07 19:34:16 +0300

    Dictionary to Store Person Info from User Input

---
+++ Practice/user_info.py
@@ -0,0 +1,10 @@
+# first an empty dictionary
+person = {}
+
+# obtain input from the user
+person["name"] = input("Enter your name: ")
+person["age"] = input("Enter your age: ")
+person["favorite_color"] = input("Enter your favorite color: ")
+
+# display the dictionary
+print("Person Information:", person)
